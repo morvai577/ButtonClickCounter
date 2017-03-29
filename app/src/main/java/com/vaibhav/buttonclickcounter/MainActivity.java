c3172ed3ff09149062bb85a65e09e1d73ad722c6
Vai Mor <mor.vaibhav577@gmail.com>
2017-03-29 17:39:16 +1300

    Added button, editText and TexView widgets

--- app/src/main/java/com/vaibhav/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/vaibhav/buttonclickcounter/MainActivity.java
@@ -2,12 +2,28 @@ package com.vaibhav.buttonclickcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Variables referring to widgets
+    private EditText userInput;
+    private Button button;
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
     }
+
+
 }
