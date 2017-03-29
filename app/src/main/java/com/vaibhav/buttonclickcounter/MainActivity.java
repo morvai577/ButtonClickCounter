81ca2133af2e09af6914d5c6a7a7265acaafd394
Vai Mor <mor.vaibhav577@gmail.com>
2017-03-29 17:58:00 +1300

    Added scrolling to textView and got rid of the initial text in textView. Also fixed some grammer mistakes in output.

--- app/src/main/java/com/vaibhav/buttonclickcounter/MainActivity.java
+++ app/src/main/java/com/vaibhav/buttonclickcounter/MainActivity.java
@@ -2,6 +2,7 @@ package com.vaibhav.buttonclickcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,11 +25,14 @@ public class MainActivity extends AppCompatActivity {
         userInput = (EditText) findViewById(R.id.editText);
         button = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView);
+        textView.setText(""); // Initialise textView with empty string
+        textView.setMovementMethod(new ScrollingMovementMethod()); // Adding scrolling functionality to textView
         View.OnClickListener ourOnClickListener = new View.OnClickListener() { // New object of type View.OnClockListener interface
             @Override
             public void onClick(View v) {
                 numTimesClicked = numTimesClicked + 1;
-                String result = "\nThe button got tapped " + numTimesClicked + " times";
+                String result = "\nThe button got tapped " + numTimesClicked + " time";
+                if(numTimesClicked != 1) {result += "s";} // this is the same as result = result + "s"
                 textView.append(result);
             }
         };
